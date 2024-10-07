11e26c685a3092230d8ac13d6c5ed78d2f9f88ca
H0peuless <hopeuleuss@gmail.com>
2024-10-07 12:02:47 -0600

    format output string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
 struct _capacitor
@@ -14,27 +15,27 @@ typedef struct _capacitor Capacitor;
 
 Capacitor createCapacitor(float dt, float finalTime, double capacitance) {
     Capacitor cap;
-    int size = finalTime/dt;
-    // Allocation de mémoire pour les tableaux
-    cap.time = (double *)malloc(size * sizeof(double));
-    cap.voltage = (double *)malloc(size * sizeof(double));
-    cap.current = (double *)malloc(size * sizeof(double));
+    int size = finalTime/dt; // Size of the arrays
+    // Table memory allocation
+    cap.time = (double *)malloc(size * sizeof(double));    // Time array
+    cap.voltage = (double *)malloc(size * sizeof(double)); // Voltage array
+    cap.current = (double *)malloc(size * sizeof(double)); // Current array
 
-    // Vérification de l'allocation
+    // Memory allocation check
     if (cap.time == NULL || cap.voltage == NULL || cap.current == NULL) {
         perror("Memory allocation error");
         exit(EXIT_FAILURE);
     }
-    cap.time[0] = 0;
-    for(int i = 1; i < size; i++) {
-        cap.time[i] = cap.time[i-1] + dt;
+    cap.time[0] = 0; // Initial value for the time array
+    for(int i = 1; i < size; i++){
+        cap.time[i] = cap.time[i-1] + dt; // Add a new instant
     }
-    cap.C = capacitance;
+    cap.C = capacitance; // Capacitance
     return cap;
 }
 
 
-void computeVoltage(Capacitor* capacitor,float V0, float I0, int time) {
+void computeVoltage(Capacitor* capacitor,float V0, float I0, int time) { //Compute the current if the voltage is constant
     capacitor->voltage[0] = 0;
     capacitor->current[1] = I0;
     double dt = capacitor->time[1] - capacitor->time[0];
@@ -44,9 +45,9 @@ void computeVoltage(Capacitor* capacitor,float V0, float I0, int time) {
     }
 }
 
-void computeCurrent(Capacitor* capacitor,float V0, float I0, float R, int time) {
+void computeCurrent(Capacitor* capacitor,float V0, float I0, float R, int time) { //Compute the current if the voltage is constant
     capacitor->voltage[0] = V0;
-    capacitor->current[0] = I0;
+    capacitor->current[0] = 0;
     double dt = capacitor->time[1] - capacitor->time[0];
     for(int i = 1; i < time; i++) {
         capacitor->current[i] = capacitor->current[i-1] - (capacitor->current[i-1]/(R*capacitor->C))*dt;
@@ -55,8 +56,8 @@ void computeCurrent(Capacitor* capacitor,float V0, float I0, float R, int time) 
 }
 
 void printCapacitor(Capacitor* capacitor,int time){
-    for(int i = 0; i < time; i+=200) {
-        cout << capacitor->time[i] << " " << capacitor->voltage[i] << " " << capacitor->current[i] << endl;
+    for(int i = 0; i < time; i+=200) { //Every 200 stps print the values
+        cout << setw(8) << left <<  capacitor->time[i] << " | "<<  setw(8) << left << capacitor->voltage[i] << " | "<<  setw(8) << left << capacitor->current[i] << endl; //Format the output string as a table
     }
 }
 
@@ -64,19 +65,19 @@ void printCapacitor(Capacitor* capacitor,int time){
 
 
 int main(int argc, char *argv[]) {
-    const double C = 100e-12;
-    const float R = 1000;
-    const float V0 = 10;
-    const float I0 = 1e-2;
+    const double C = 100e-12; // Capacitance
+    const float R = 1000;     // Resistance in circuit 2
+    const float V0 = 10;      // Voltage in constant voltage circuit
+    const float I0 = 1e-2;    // Current in constant current circuit
     const float finalTime = 5e-6;
     const float dt = 1e-10;
     const int time = int(finalTime/dt);
 
     Capacitor C1 = createCapacitor(dt,finalTime, C);
 
-    computeCurrent(&C1,V0,I0,R,time);
-    computeVoltage(&C1,V0,I0,time);
-    printCapacitor(&C1,time);
+    computeCurrent(&C1,V0,I0,R,time); //Compute the current
+    computeVoltage(&C1,V0,I0,time);   //Compute the voltage
+    printCapacitor(&C1,time);         //print method every 200 steps
 
     return 0;
 };
